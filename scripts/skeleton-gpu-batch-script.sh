5d6df93417a5b67acd0ee3a10080f835f96b0db5
Akshay Mhatre <35747718+akshay1929@users.noreply.github.com>
2022-10-25 23:31:44 -0700

    Res

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -15,7 +15,15 @@ export LIBOMPTARGET_INFO=4
 # it is assumed your environment is set up properly for using the Cori GPUs
 # prior to you launching this batch script
 #
-nvprof -m sm_efficiency ./sobel_gpu
+for N in 32 64 128 256 512 1024 # loop over problem sizes
+   do
+    for t in 1 4 16 64 256 1024 4096  # loop over concurrency level
+#      for t in 1  # if running the blas version, uncomment this line and comment out the previous line looping over t in 1 4 16 64
+        do
+        echo nvprof -m sm_efficiency ./sobel_gpu $N $t
+        done 
+done # iterate over problem size
+
 
 nvprof -m sm_efficiency ./sobel_cpu_omp_offload
 
