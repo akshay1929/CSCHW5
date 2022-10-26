cc66a8e39c4cba8d9e5e91a51cadceb0a1ce3ce5
Akshay Mhatre <35747718+akshay1929@users.noreply.github.com>
2022-10-26 00:36:03 -0700

    Res

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -15,24 +15,16 @@ export LIBOMPTARGET_INFO=4
 # it is assumed your environment is set up properly for using the Cori GPUs
 # prior to you launching this batch script
 #
-#Runtime
 for N in 32 64 128 256 512 1024 # loop over number of thread blocks
    do
     for t in 1 4 16 64 256 1024 4096  # loop over number of threads per block
 #      for t in 1  # if running the blas version, uncomment this line and comment out the previous line looping over t in 1 4 16 64
         do
+        #Runtime
         echo nvprof ./sobel_gpu $N $t
         nvprof ./sobel_gpu $N $t
-        echo 
-        done #iterate over number of threads per block 
-done # iterate over number of thread blocks
-
-# Efficiency
-for N in 32 64 128 256 512 1024 # loop over number of thread blocks
-   do
-    for t in 1 4 16 64 256 1024 4096  # loop over number of threads per block
-#      for t in 1  # if running the blas version, uncomment this line and comment out the previous line looping over t in 1 4 16 64
-        do
+        echo
+        #Efficiency
         echo nvprof ./sobel_gpu $N $t
         nvprof -m sm_efficiency ./sobel_gpu $N $t
         echo 
